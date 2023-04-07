97919f3748b44e8f2f435545654211b5a8b10632
Zofia Oknińska <zofia.okninska@microsoft.wsei.edu.pl>
2023-04-07 19:38:22 +0200

    Override statistics methods on RankedGradeBook class to check whether there are at least 5 students to proceed

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,5 +33,27 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+            {
+                Console.Write("Ranked grading requires at least 5 students.");
+                return;
+            }
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5) 
+            { 
+                Console.Write("Ranked grading requires at least 5 students.");
+                return; 
+            }
+            else
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
